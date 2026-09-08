cac078030608010ef3fe408ed633a8f62f3aa14b
Laith.erp <309757272+www031827-lang@users.noreply.github.com>
2026-09-08 01:43:45 +0000

    Add LoginResponse and related data classes

--- android/siraj-general-manager/src/main/data/models/LoginResponse.kt
+++ android/siraj-general-manager/src/main/data/models/LoginResponse.kt
@@ -1,1 +1,26 @@
+package com.siraj.erp.generalmanager.data.models
 
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(
+    val status: String,
+    val message: String,
+    val data: UserData?
+)
+
+data class UserData(
+    val user: UserInfo,
+    @SerializedName("allowed_apps")
+    val allowedApps: List<String>,
+    val permissions: List<String>,
+    val token: String
+)
+
+data class UserInfo(
+    val id: String,
+    val full_name: String,
+    val username: String,
+    val email: String?,
+    val role: String?,
+    val status: String
+)
