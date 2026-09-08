4344934568d79ca6eaf5d4987bb6f5a96f3fe176
Laith.erp <309757272+www031827-lang@users.noreply.github.com>
2026-09-08 01:46:14 +0000

    Add ApiService interface for login functionality

--- android/siraj-general-manager/src/main/data/api/ApiService.kt
+++ android/siraj-general-manager/src/main/data/api/ApiService.kt
@@ -1,1 +1,12 @@
+package com.siraj.erp.generalmanager.data.api
 
+import com.siraj.erp.generalmanager.data.models.LoginRequest
+import com.siraj.erp.generalmanager.data.models.LoginResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiService {
+    @POST("api/auth/login")
+    suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
+}
